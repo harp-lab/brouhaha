80b0af9846783114d8be684e44e45c91c275b5cb
Michaelgathara <mikegtrm@gmail.com>
2023-05-12 17:57:44 -0500

    added file output option

--- test.rkt
+++ test.rkt
@@ -5,31 +5,33 @@
 
 (require "compile.rkt")
 
+; Read all expressions from a file
 (define (read-program program)
   (with-input-from-file program
     (lambda ()
       (read-all (current-input-port)))))
 
+; Helper function to read all expressions from an input port
 (define (read-all in)
-  (let ([next (read in)])
-    (if (eof-object? next)
-        '()
-        (cons next (read-all in)))))
+  (let loop ([exprs '()])
+    (let ([next (read in)])
+      (if (eof-object? next)
+          (reverse exprs)
+          (loop (cons next exprs))))))
 
-(define (run-program program)
-  (with-output-to-file "output"
+; Run and compile a program, writing the output to a file
+(define (run-program program filename)
+  (displayln (~a "Now running: " filename))
+  (with-output-to-file (string-append filename "_output")
     (lambda ()
       (print (compile program)))))
 
+; Read the directory and process all .haha files
 (define (read-direc directory)
   (for ([file (in-list (directory-list directory))])
-    (displayln (~a "files: " file))
     (let ([full-path (build-path directory file)])
       (when (and (file-exists? full-path)
                  (regexp-match? #rx"[.]haha$" file))
-        (run-program (read-program full-path))))))
+        (run-program (read-program full-path) file)))))
 
-(trace read-direc)
-(trace read-program)
-(trace read-all)
 (read-direc "tests/")
