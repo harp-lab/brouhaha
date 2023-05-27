318ddbb9296dad7c406e1464b3861fe809166558
michaelgathara <mikegtrm@gmail.com>
2023-05-26 20:07:15 -0500

    stuck on cps conversion interp

--- interp.rkt
+++ interp.rkt
@@ -7,7 +7,7 @@
 (provide interp)
 
 (define prelude (read-program (build-path (current-directory) "prelude.haha")))
-(define program (read-program (build-path (current-directory) "tests" "easy.haha")))
+(define program (read-program (build-path (current-directory) "tests" "foldl-style.haha")))
 (define program_exp (cps-convert (anf-convert (alphatize (desugar (append prelude program))))))
 
 
@@ -20,17 +20,21 @@
         [`() env+])))
 
   (define (eval exp env)
+    ; (displayln (~a "env " (pretty-print env) "\n\nand exp " exp))
     (match exp
+      [(? number? x) x]
       [`(quote ,(? number? x)) x]
       [`(quote ,(? boolean? x)) x]
       [`(quote ,(? symbol? x)) x]
       [(? symbol?) (hash-ref env exp)]
       [`(prim ,op ,es ...)
+        (displayln (~a "Prim op es: " exp))
         (apply (racket-eval op (make-base-namespace)) (map (lambda (e) (eval e env)) es))]
-      [`(apply-prim ,op ,e0) 
-        (apply (racket-eval op (make-base-namespace)) (eval e0 env))]
       [`(apply-prim halt ,lst)
-        (pretty-print "HI")]
+        (hash-ref env lst)]
+      [`(apply-prim ,op ,e0) 
+        (displayln (~a "\n\nApply-prim op e0: " exp "\n and env: " (pretty-print env)))
+        (apply (racket-eval op (make-base-namespace)) (eval e0 env))]
       [`(lambda ,_ ,_) 
         `(closure ,exp ,env)]
       [`(if ,ec ,et ,ef)
@@ -38,23 +42,23 @@
          (if val (eval et env) (eval ef env)))]
       [`(let ([,xs ,rhss] ...) ,body)
         ; (pretty-print exp)
+        ; (pretty-print env)
         (eval body (foldl (lambda (x rhs env+) (hash-set env+ x (eval rhs env))) env xs rhss))]
       [`(let* ([,xs ,rhss] ...) ,body)
         ; (pretty-print exp)
         (eval body (foldl (lambda (x rhs env+) (hash-set env+ x (eval rhs env+))) env xs rhss))]
       ; [`(halt ,x ,y) (displayln (~a "Halt case: " x " and y " y))]
       [`(,ef ,eas ...)
-        (displayln (~a "Im in the app case"))
+        ; (displayln (~a "Im in the app case"))
         (let ([fn-val (eval ef env)]
               [arg-vals 
                 (map (lambda (ea) (eval ea env)) eas)])
           (appl fn-val arg-vals))]))
 
   (define (appl fn-val arg-vals)
-    (displayln (~a "Im in the appl case with " fn-val " and " arg-vals))
+    ; (displayln (~a "Im in the appl case with " fn-val " and " arg-vals))
     (match fn-val
-      ; [`(halt ,ef ,ea)
-      ;   (eval ea )]
+      [(? number? x) (cadr arg-vals)]
       [`(closure (lambda (,xs ...) ,eb) ,env)
        (eval eb (foldl (lambda (x val env) (hash-set env x val)) env xs arg-vals))]
       [`(closure (lambda ,x ,eb) ,env)
