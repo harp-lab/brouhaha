264f598f12517ab0296d99e325fd167b0966c1bc
Akshar Patel <aksharmpatel1@gmail.com>
2023-06-06 20:32:09 -0500

    started emit-slog.rkt

--- emit-slog.rkt
+++ emit-slog.rkt
@@ -2,50 +2,23 @@
 
 ;; This program will take in a brouhaha input and convert it to a slog-compatible database
 
-
-
-(define (alphatize program)
-  (define rename gensym)
-  (define ((alpha-rename env) e)
-    (match e
-      [`(let ([,xs ,es] ...) ,e0)
-       (define xs+ (map rename xs))
-       (define env+ (foldl (lambda (x x+ env) (hash-set env x x+)) env xs xs+))
-       `(let ,(map list xs+ (map (alpha-rename env) es)) ,((alpha-rename env+) e0))]
-      [`(lambda (,xs ...) ,e0)
-       (define xs+ (map rename xs))
-       (define env+ (foldl (lambda (x x+ env) (hash-set env x x+)) env xs xs+))
-       `(lambda ,xs+ ,((alpha-rename env+) e0))]
-      [`(lambda ,x ,e0)
-       (define x+ (rename x))
-       (define env+ (hash-set env x x+))
-       `(lambda ,x+ ,((alpha-rename env+) e0))]
-      [`(prim ,op ,es ...) `(prim ,op ,@(map (alpha-rename env) es))]
-      [`(apply-prim ,op ,e0) `(apply-prim ,op ,((alpha-rename env) e0))]
-      [`(if ,e0 ,e1 ,e2)
-       `(if ,((alpha-rename env) e0) ,((alpha-rename env) e1) ,((alpha-rename env) e2))]
-      [`(apply ,e0 ,e1) `(apply ,((alpha-rename env) e0) ,((alpha-rename env) e1))]
-      [(? symbol? x) (hash-ref env x)]
-      [`',dat `',dat]
-      [`(,es ...) (map (alpha-rename env) es)]))
-  (define ((rename-define env) def)
-    (match def
-      [`(define (,fname ,params ...)
-          ,body)
-       `(define (,fname ,@params)
-          ,((alpha-rename (foldl (lambda (x h) (hash-set h x x)) env params)) body))]
-      [`(define (,fname . ,(? symbol? params))
-          ,body)
-       `(define (,fname . ,params)
-          ,((alpha-rename (hash-set env params params)) body))]))
-  (define top-env
-    (foldl (lambda (def env)
-             (match def
-               [`(define (,fname . ,params)
-                   ,body)
-                (when (hash-has-key? env fname)
-                  (error "Function name is already defined"))
-                (hash-set env fname fname)]))
-           (hash)
-           program))
-  (map (rename-define top-env) program))
+(define (write-program-for-slog program)
+  (define (sym->qstr x) (format "\"~a\"" (symbol->string x)))
+  (define (write-def def)
+    (match def [`(define (,fname ,(? symbol? xs) ...) ,body)
+                (format "(top-level-define (define ~a (fixedparam [~a]) ~a))" fname (foldr string-append "" (map sym->qstr xs)) (write-exp body))] 
+               ...))
+  (define (write-exp exp)
+    (match exp
+           [`(lambda (,(? symbol? xs) ...) ,body)
+            (foldr string-append "" `("(lambda (fixedparam [" ,@(map sym->qstr xs) "]) " ,(write-exp body) ")"))]
+           [(? symbol? x) 
+            (format "(ref \"~a\")" x)]
+           [(? boolean? x)
+            (if x
+             "(bool \"t\")"
+             "(bool \"f\")")]
+           [(? number?)
+            (format "(const \"~a\")")]))
+  (display (foldr string-append (map write-def program)))
+  (void))
