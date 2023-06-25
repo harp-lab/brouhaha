66a67e5c004e5e33c2d18c56ea39f4e9beaac822
michaelgathara <mikegtrm@gmail.com>
2023-06-25 04:01:54 -0500

    automatic driver and cmake outputs

--- tests/error/driver.cpp
+++ tests/error/driver.cpp
@@ -1,54 +1,63 @@
 #include <prelude.hpp>
-#include <testing.hpp>
-#include <filesystem>
-#include <gtest/gtest.h>
-#include <iostream>
+        #include <testing.hpp>
+        #include <filesystem>
+        #include <gtest/gtest.h>
+        #include <iostream>
 
-TEST(and_Test, desugar)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string desugar_res = readFileToString("../../../tests/and/output/and_alphatize_res.out");
-    ASSERT_EQ(answer, desugar_res);
-}
+        std::string getCurrentDirectory() {
+            return std::filesystem::path(__FILE__).parent_path().filename();
+        }
 
-TEST(and_Test, alphatize)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string alphatize_res = readFileToString("../../../tests/and/output/and_alphatize_res.out");
-    ASSERT_EQ(answer, alphatize_res);
-}
+        std::string constructRelativePath(const std::string& subpath) {
+            return "../../../tests/" + getCurrentDirectory() + "/" + subpath;
+        }
 
-TEST(and_Test, anf)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string anf_res = readFileToString("../../../tests/and/output/and_anf_res.out");
-    ASSERT_EQ(answer, anf_res);
-}
+        TEST(and_Test, desugar)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string desugar_res = readFileToString(constructRelativePath("output/and_alphatize_res.out"));
+            ASSERT_EQ(answer, desugar_res);
+        }
 
-TEST(and_Test, closure)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string closure_res = readFileToString("../../../tests/and/output/and_closure_res.out");
-    ASSERT_EQ(answer, closure_res);
-}
+        TEST(and_Test, alphatize)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string alphatize_res = readFileToString(constructRelativePath("output/and_alphatize_res.out"));
+            ASSERT_EQ(answer, alphatize_res);
+        }
 
-TEST(and_Test, cps)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string cps_res = readFileToString("../../../tests/and/output/and_cps_res.out");
-    ASSERT_EQ(answer, cps_res);
-}
+        TEST(and_Test, anf)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string anf_res = readFileToString(constructRelativePath("output/and_anf_res.out"));
+            ASSERT_EQ(answer, anf_res);
+        }
 
-TEST(and_Test, CPP_test)
-{
-    std::string answer = readFileToString("../../../tests/and/answer");
-    std::string output = executeAndGetOutput("./and_exec");
-    writeStringToFile("../../../tests/and/output/and_cpp_res.out",output);
-    ASSERT_EQ(answer, output);
-}
+        TEST(and_Test, closure)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string closure_res = readFileToString(constructRelativePath("output/and_closure_res.out"));
+            ASSERT_EQ(answer, closure_res);
+        }
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+        TEST(and_Test, cps)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string cps_res = readFileToString(constructRelativePath("output/and_cps_res.out"));
+            ASSERT_EQ(answer, cps_res);
+        }
+
+        TEST(and_Test, CPP_test)
+        {
+            std::string answer = readFileToString(constructRelativePath("answer"));
+            std::string output = executeAndGetOutput("./and_exec");
+            writeStringToFile(constructRelativePath("output/and_cpp_res.out"), output);
+            ASSERT_EQ(answer, output);
+        }
+
+        int main(int argc, char** argv)
+        {
+            ::testing::InitGoogleTest(&argc, argv);
+            return RUN_ALL_TESTS();
+        }
+        
