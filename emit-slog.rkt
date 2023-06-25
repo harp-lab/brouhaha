617d846be0e5db984d6ad7a99af27add7aeb2d7a
michaelgathara <mikegtrm@gmail.com>
2023-06-25 02:36:12 -0500

    further test.rkt improvements

--- emit-slog.rkt
+++ emit-slog.rkt
@@ -5,65 +5,75 @@
 ;; This program will take in a brouhaha input and convert it to a slog-compatible database
 
 (define (write-program-for-slog program)
-  (define (sym->qstr x) (format "\"~a\"" (symbol->string x)))
+  (define (sym->qstr x)
+    (format "\"~a\"" (symbol->string x)))
   (define (write-def def)
-    (match def 
-      [`(define (,fname ,(? symbol? xs) ...) ,body)
-       (format "(store (addr \"~a\") (define \"~a\" (fixedparam [~a]) ~a))\n" fname fname (foldr string-append "" (map sym->qstr xs)) (write-exp body))] 
-      [`(define (,fname . ,(? symbol? params)) ,body)
-       (format "(store (addr \"~a\") (define \"~a\" (varparam ~a) ~a))\n" fname fname (sym->qstr params) (write-exp body))]
-    )
-  )
+    (match def
+      [`(define (,fname ,(? symbol? xs) ...)
+          ,body)
+       (format "(store (addr \"~a\") (define \"~a\" (fixedparam [~a]) ~a))\n"
+               fname
+               fname
+               (foldr string-append "" (map sym->qstr xs))
+               (write-exp body))]
+      [`(define (,fname . ,(? symbol? params))
+          ,body)
+       (format "(store (addr \"~a\") (define \"~a\" (varparam ~a) ~a))\n"
+               fname
+               fname
+               (sym->qstr params)
+               (write-exp body))]))
   (define (write-exp exp [ref? #t])
     (match exp
-           [`',e (format "(quote ~a)" (write-exp e))]
-           [(? symbol? x)
-              (if ref?
-               (format "(ref \"~a\")" x)
-               (format "\"~a\"" x))]
-           [(? boolean? x)
-            (if x
-             "(bool \"t\")"
-             "(bool \"f\")")]
-           [(? number? x)
-            (format "(const \"~a\")" x)]
-           [`(lambda (,(? symbol? xs) ...) ,body)
-            (foldr string-append "" `("(lambda (fixedparam [" ,@(map sym->qstr xs) "]) " ,(write-exp body) ")"))]
-           [`(lambda ,(? symbol? x) ,body)
-            (foldr string-append "" `("(lambda (varparam [" ,(sym->qstr x) "]) " ,(write-exp body) ")"))]
-           [`(let ([,xs ,es] ...) ,body)
-            (foldr string-append "" `("(let [" ,(foldr (lambda (x e acc) (string-append (format " (binding ~a ~a)" (sym->qstr x) (write-exp e #f)) acc)) "" xs es) "] " ,(write-exp body) ")"))]
-           [`(if ,grd ,tExp ,fExp)
-            (foldr string-append "" `("(if " ,(write-exp grd) " " ,(write-exp tExp) " " ,(write-exp fExp) ")"))]
-           [`(apply-prim ,op ,e1)
-            (foldr string-append "" `("(apply-prim \"" ,(~a op) "\" " ,(write-exp e1) ")"))]
-           [`(prim ,op ,es ...)
-            (foldr string-append "" `("(prim \"" ,(~a op) "\" [" ,@(map write-exp es) "])"))]
-           [`(apply ,e0 ,e1)
-            (foldr string-append "" `("(appl " ,(write-exp e0) " " ,(write-exp e1) ")"))]
-           [`(,ef ,eas ...)
-            (foldr string-append "" `("(app " ,(write-exp ef) " [" ,@(map write-exp eas) "])"))]
-    )
-  )
+      [`',e (format "(quote ~a)" (write-exp e))]
+      [(? symbol? x) (if ref? (format "(ref \"~a\")" x) (format "\"~a\"" x))]
+      [(? boolean? x) (if x "(bool \"t\")" "(bool \"f\")")]
+      [(? number? x) (format "(const \"~a\")" x)]
+      [`(lambda (,(? symbol? xs) ...) ,body)
+       (foldr string-append
+              ""
+              `("(lambda (fixedparam [" ,@(map sym->qstr xs) "]) " ,(write-exp body) ")"))]
+      [`(lambda ,(? symbol? x) ,body)
+       (foldr string-append "" `("(lambda (varparam [" ,(sym->qstr x) "]) " ,(write-exp body) ")"))]
+      [`(let ([,xs ,es] ...) ,body)
+       (foldr string-append
+              ""
+              `("(let [" ,(foldr (lambda (x e acc)
+                                   (string-append
+                                    (format " (binding ~a ~a)" (sym->qstr x) (write-exp e #f))
+                                    acc))
+                                 ""
+                                 xs
+                                 es)
+                         "] "
+                         ,(write-exp body)
+                         ")"))]
+      [`(if ,grd ,tExp ,fExp)
+       (foldr string-append
+              ""
+              `("(if " ,(write-exp grd) " " ,(write-exp tExp) " " ,(write-exp fExp) ")"))]
+      [`(apply-prim ,op ,e1)
+       (foldr string-append "" `("(apply-prim \"" ,(~a op) "\" " ,(write-exp e1) ")"))]
+      [`(prim ,op ,es ...)
+       (foldr string-append "" `("(prim \"" ,(~a op) "\" [" ,@(map write-exp es) "])"))]
+      [`(apply ,e0 ,e1) (foldr string-append "" `("(appl " ,(write-exp e0) " " ,(write-exp e1) ")"))]
+      [`(,ef ,eas ...)
+       (foldr string-append "" `("(app " ,(write-exp ef) " [" ,@(map write-exp eas) "])"))]))
   (define (emit-top-level-env program)
     (format "(top-level-env ~a)"
-      (foldr 
-      (lambda (def code) 
-                (match def
-                  [`(define (,fname . ,_) ,_)
-                    (format "(env-set ~a \"~a\" (addr \"~a\"))"
-                    code
-                    fname
-                    fname)]))
-      "(empty)"
-      program)))
+            (foldr (lambda (def code)
+                     (match def
+                       [`(define (,fname . ,_)
+                           ,_)
+                        (format "(env-set ~a \"~a\" (addr \"~a\"))" code fname fname)]))
+                   "(empty)"
+                   program)))
 
   (display (foldr string-append (emit-top-level-env program) (map write-def program)))
   (void))
 
-
 ; (write-program-for-slog `((define (call num1 num2) (let ((x83113 num1) (y83114 num2)) x83113))
-  ; (define (brouhaha_main) (call '5 '42))))
+; (define (brouhaha_main) (call '5 '42))))
 
 ;;; (write-program-for-slog `((define (brouhaha_main) ((lambda (x) x) 42))))
 
--- test.rkt
+++ test.rkt
@@ -1,11 +1,25 @@
 #lang racket
 
-(require "compile.rkt")
-(require "interp-anf.rkt")
-(require "interp-cps.rkt")
-(require "interp-closure.rkt")
-(require "emit-cpp.rkt")
-(require "emit-slog.rkt")
+(require "compile.rkt"
+         "utils.rkt"
+         "interp-anf.rkt"
+         "interp-cps.rkt"
+         "interp-closure.rkt"
+         "emit-cpp.rkt"
+         "emit-slog.rkt")
+
+(define (print-color text color)
+  (display "\x1b[")
+  (display color)
+  (display "m")
+  (display text)
+  (display "\x1b[0m"))
+
+(define (print-red text)
+  (print-color text "31"))
+
+(define (print-green text)
+  (print-color text "32"))
 
 (define (write-to file content)
   (with-output-to-file file (lambda () (pretty-print content)) #:exists 'replace))
@@ -13,75 +27,87 @@
 (define (verify-correctness file desugar alphatize anf cps closure)
   (cond
     [(and (equal? desugar alphatize) (equal? alphatize anf) (equal? anf cps) (equal? cps closure))
-     (displayln (~a "Each output stage matched!\n"))]
-    [else (displayln (~a "Your outputs did not match for " file "\n"))]))
+     (print-green "Each output stage matched!")
+     (displayln "\n")]
+    [else
+     (print-red "Your outputs did not match for")
+     (display (~a file "\n"))]))
 
 (define (verify-dir out-dir)
   (unless (directory-exists? out-dir)
     (pretty-print out-dir)
     (make-directory out-dir)))
 
+(define (verify-file cmake-file)
+  (unless (file-exists? cmake-file)
+    ('todo)))
+
 (define (run-program directory program filename file-path prelude-path)
-  (pretty-print (list directory program filename file-path prelude-path))
-  (define filename-string-ext
-    (path->string filename)) ; converting  #<path:apply.haha> to "apply.haha"
-  (define filename-string (regexp-replace #rx"[.]haha$" filename-string-ext "")) ; fname wo extension
-  (define out-dir (string-append directory "/" filename-string)) ; "tests2/" + "apply"
-  (verify-dir out-dir)
+  (with-handlers ([exn:fail?
+                   (lambda (exn)
+                     (displayln
+                      (~a "Now running: " filename "\n" "Failed to run: " filename " with error "))
+                     (print-red (exn-message exn))
+                     (displayln "\n"))])
+    
+    (define filename-string-ext
+      (path->string filename)) ; converting  #<path:apply.haha> to "apply.haha"
+    (define filename-string
+      (regexp-replace #rx"[.]haha$" filename-string-ext "")) ; fname wo extension
+    (define out-dir (string-append directory "/" filename-string)) ; "tests2/" + "apply"
 
-  (let* ([generate-res-filepath
-          (lambda (suffix)
-            (string-append
-             out-dir
-             "/output/"
-             filename-string
-             suffix))] 
-         [generate-comp-filepath
-          (lambda (suffix) (string-append out-dir "/compiler-out/" filename-string suffix))]
-         [compiled-program (compile (append (read-program prelude-path)
-                                            (read-program file-path)))] 
-         [desugar_prg (list-ref compiled-program 0)]
-         [alphatize_prg (list-ref compiled-program 1)]
-         [anf_prg (list-ref compiled-program 2)]
-         [cps_prg (list-ref compiled-program 3)]
-         [cps_after_anf (list-ref compiled-program 4)]
-         [clo_conv_prg (list-ref compiled-program 5)]
-         [results (list (interp desugar_prg)
-                        (interp alphatize_prg)
-                        (interp anf_prg)
-                        (interp-cps cps_prg)
-                        (interp-closure clo_conv_prg))])
+    (verify-cmake (string-append out-dir "CMakeLists.txt"))
 
-    (verify-dir out-dir)
-    (verify-dir (string-append out-dir "/output/"))
-    (verify-dir (string-append out-dir "/compiler-out/"))
-    (displayln (~a "Now running: " filename-string))
+    (let* ([generate-res-filepath
+            (lambda (suffix) (string-append out-dir "/output/" filename-string suffix))]
+           [generate-comp-filepath
+            (lambda (suffix) (string-append out-dir "/compiler-out/" filename-string suffix))]
+           [compiled-program (compile (append (read-program prelude-path) (read-program file-path)))]
+           [desugar_prg (list-ref compiled-program 0)]
+           [alphatize_prg (list-ref compiled-program 1)]
+           [anf_prg (list-ref compiled-program 2)]
+           [cps_prg (list-ref compiled-program 3)]
+           [cps_after_anf (list-ref compiled-program 4)]
+           [clo_conv_prg (list-ref compiled-program 5)]
+           [results (list (interp desugar_prg)
+                          (interp alphatize_prg)
+                          (interp anf_prg)
+                          (interp-cps cps_prg)
+                          (interp-closure clo_conv_prg))])
 
-    (displayln (~a "Emitting C++ for: "
-                   filename-string
-                   " and outputting to: "
-                   (generate-comp-filepath "_cpp_program.cpp")))
-    (emit-cpp clo_conv_prg (generate-comp-filepath "_cpp_program.cpp"))
+      (verify-dir out-dir)
+      (verify-dir (string-append out-dir "/output/"))
+      (verify-dir (string-append out-dir "/compiler-out/"))
+      (displayln (~a "Now running: " filename-string))
 
-    ; (displayln (~a "Emitting Slog for: "
-    ;                filename-string
-    ;                " and outputting to: "
-    ;                (generate-filepath "_slog.slog")))
-    ; ; (write-to (generate-filepath "_slog.slog") (write-program-for-slog desugar_prg))
-    ; (with-output-to-file (generate-filepath "_slog.slog") (lambda () (pretty-print (write-program-for-slog desugar_prg))) #:exists 'replace)
+      (displayln (~a "Emitting C++ for: "
+                     filename-string
+                     " and outputting to: "
+                     (generate-comp-filepath "_cpp_program.cpp")))
+      (emit-cpp clo_conv_prg (generate-comp-filepath "_cpp_program.cpp"))
 
-    (for-each
-     write-to
-     (map generate-comp-filepath
-          (list "_compile.out" "_desugar.out" "_alphatize.out" "_anf.out" "_cps.out" "_closure.out"))
-     compiled-program)
-    (for-each
-     write-to
-     (map
-      generate-res-filepath
-      (list "_desugar_res.out" "_alphatize_res.out" "_anf_res.out" "_cps_res.out" "_closure_res.out"))
-     results)
-    (apply verify-correctness (cons filename-string results))))
+      ; (displayln (~a "Emitting Slog for: "
+      ;                filename-string
+      ;                " and outputting to: "
+      ;                (generate-filepath "_slog.slog")))
+      ; ; (write-to (generate-filepath "_slog.slog") (write-program-for-slog desugar_prg))
+      ; (with-output-to-file (generate-filepath "_slog.slog") (lambda () (pretty-print (write-program-for-slog desugar_prg))) #:exists 'replace)
+
+      (for-each
+       write-to
+       (map
+        generate-comp-filepath
+        (list "_compile.out" "_desugar.out" "_alphatize.out" "_anf.out" "_cps.out" "_closure.out"))
+       compiled-program)
+      (for-each write-to
+                (map generate-res-filepath
+                     (list "_desugar_res.out"
+                           "_alphatize_res.out"
+                           "_anf_res.out"
+                           "_cps_res.out"
+                           "_closure_res.out"))
+                results)
+      (apply verify-correctness (cons filename-string results)))))
 
 (define (read-direc directory)
   (for-each (lambda (dir)
