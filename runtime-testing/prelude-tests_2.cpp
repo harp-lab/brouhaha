ead61efd06c4e786b8e4d5995c789b59a25dd065
SowmithK <skunapan@uab.edu>
2023-06-26 18:33:34 -0500

    Made changes to prelude.hpp to change how the stdout works for brouhaha, to o/p stuff in the racket format

--- runtime-testing/prelude-tests_2.cpp
+++ runtime-testing/prelude-tests_2.cpp
@@ -1,26 +1,6 @@
 #include <gtest/gtest.h>
 #include <../prelude.hpp>
 
-TEST(MultiplyTests, TestIntegerOne_One)
-{
-    const auto expected = 1;
-    const auto actual   = test_lol(1, 1);
-    ASSERT_EQ(expected, actual);
-}
-
-TEST(MultiplyTests, TestIntegerZero_Zero)
-{
-    const auto expected = 0;
-    const auto actual   = test_lol(0, 0);
-    ASSERT_EQ(expected, actual);
-}
-
-// TEST(MultiplyTests, TestIntegerZero_One)
-// {
-//     const auto expected = 0;
-//     const auto actual   = multiply(0, 1);
-//     ASSERT_EQ(actual, expected);
-// }
 
 int main(int argc, char** argv)
 {
