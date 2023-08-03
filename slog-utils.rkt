b8015f980c41b994ebc933bc7113fc5791dbd8ac
Sowmith Kunapaneni <skunapan@uab.edu>
2023-08-03 16:13:36 -0500

    added function for file to hash string

--- slog-utils.rkt
+++ slog-utils.rkt
@@ -1,9 +1,21 @@
 #lang racket
 
-(define (runslog slog-path out-path fact-file)
-    ;slog-path is the generated slog file
-    ;out-path is the generated out-path
-    ;fact-file is the file holding all the eval outs
+(require file/sha1)
 
-    
-    )
+(define (runslog slog-path out-path fact-file)
+  ;slog-path is the generated slog file
+  ;out-path is the generated out-path
+  ;fact-file is the file holding all the eval outs
+  'todo
+
+  )
+
+
+; ; Example usage:
+; (define file-path "test.rkt")
+; (define hash-value (file-to-sha256 file-path))
+; (printf "Hash-value of ~a: ~a\n" file-path hash-value)
+
+(define (file-to-hash-string file-path)
+  (bytes->hex-string (sha224-bytes (file->bytes file-path))))
+
