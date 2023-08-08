f115350e1f552a7e67fd3d56450da73e70edfa2c
Sowmith Kunapaneni <skunapan@uab.edu>
2023-08-08 14:59:52 -0500

    chages to slog-utils

--- slog-utils.rkt
+++ slog-utils.rkt
@@ -10,6 +10,9 @@
   ;fact-file is the file holding all the eval outs
   'todo)
 
+(define (file-to-hash-string file-path)
+  (bytes->hex-string (sha224-bytes (file->bytes file-path))))
+
 (define (create-line-hash file-path)
   (foldl add-line-hash (hash) (file->lines file-path)))
 
@@ -17,57 +20,27 @@
   (define items (string-split line "\t"))
   (define index (string->number (substring (car items) 1)))
   (hash-set line-hash index (read (open-input-string (string-replace (cadr items) "#" ".#"))))
-  ; (hash-set line-hash index (cadr items))
   )
 
-; ; Example usage:
-; (define file-path "test.rkt")
-; (define hash-value (file-to-sha256 file-path))
-; (printf "Hash-value of ~a: ~a\n" file-path hash-value)
-
-(define (file-to-hash-string file-path)
-  (bytes->hex-string (sha224-bytes (file->bytes file-path))))
-
-; write a function that takes a one line(string) of slog fact/s and return the list of tokens
-; assumes that the lines don't have any ",", "`", "'".
-; not writing to_tokens at this point as the facts are converted directly to s-expr
-
-(define (get-tree-hash tree)
-  (match tree
-    [`(ASThead ,tree-hash) tree-hash]
-    [`(,(? symbol? head) ,(? cons? tree-list) ,(? hash? tree-hash)) (tree-list tree-hash)]))
-
-; taking in the line and the tree, if the tree is not passed it is created anew
-; check if the first tag is already a child of main, if yes switch to that node and continue
-; if not a child, create a new node in the tree for the main, and keep going
-; (define (add-line line [tree (make-tree)])
-;   'todo)
-
-; (define (insert-item fact-rest tree)
-;   (foldl (lambda ())))
-
-; check if the first element, if it exists in the root
 (define (insert-fact fact index tree)
-  ; (define _ (p-dbg tree))
-  (define _ (p-dbg index))
-  (define temp (p-dbg fact))
+  ; (define _ (p-dbg index))
+  ; (define temp (p-dbg fact))
   (define (insert-helper fact tree-hash index)
     (match fact
       [`(,head ,child ..1)
        (if (hash-has-key? tree-hash head)
-             ; if the hash-key exists and the child is not a symbol
-             (hash-set tree-hash head (insert-fact (car child) index (hash-ref tree-hash head)))
-             (let ([node `(,head ,(hash))])
-               (hash-set tree-hash head (insert-fact (p-dbg (car child)) index node))))]
+           ; if the hash-key exists and the child is not a symbol
+           (hash-set tree-hash head (insert-fact (car child) index (hash-ref tree-hash head)))
+           (let ([node `(,head ,(hash))])
+             (hash-set tree-hash head (insert-fact (p-dbg (car child)) index node))))]
       [(or `(,fact) (? symbol? fact) (? number? fact))
-       (if (hash-has-key? tree-hash (p-dbg fact))
+       (if (hash-has-key? tree-hash fact)
            ; if the hash-key exists and the child is a symbol
            (hash-set tree-hash fact (cons (hash-ref tree-hash fact) index))
            (let ([node `(,fact ,index)]) (hash-set tree-hash fact node)))]
       ))
-  ; (match (p-dbg tree)
   (match tree
-    [`(,tree-head ,tree-hash) `(,tree-head ,(insert-helper (p-dbg fact) tree-hash index))]
+    [`(,tree-head ,tree-hash) `(,tree-head ,(insert-helper fact tree-hash index))]
     [`(,tree-head ,tree-hash ,line-hash)
      `(,tree-head ,(insert-helper fact tree-hash index) ,line-hash)]))
 
@@ -82,4 +55,20 @@
   (define index-list (hash-keys line-hash #t))
   (foldl insert-fact root-tree facts-list index-list))
 
-(slog-main "facts.txt")
+(define (get-child tree-hash child)
+  (if (hash-has-key? tree-hash child)
+      (hash-ref tree-hash child)
+      #f)
+  )
+
+; search string should be in the format
+(define (search-facts tree search-str)
+  (define items (string-split search-str "/"))
+  (foldl (lambda (item tree)
+           ; check if the item exists in the tree-hash if it does return that, if it doesn't return #f
+           )
+           )
+  'todo)
+
+(search-facts 'todo "/apply/define/clo")
+; (slog-main "facts.txt")
