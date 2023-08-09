838e85eac0ba8118f2f97e5004bd6cf35903ea5a
Sowmith Kunapaneni <skunapan@uab.edu>
2023-08-08 19:47:08 -0500

    search works,have to clean up

--- slog-utils.rkt
+++ slog-utils.rkt
@@ -32,12 +32,12 @@
            ; if the hash-key exists and the child is not a symbol
            (hash-set tree-hash head (insert-fact (car child) index (hash-ref tree-hash head)))
            (let ([node `(,head ,(hash))])
-             (hash-set tree-hash head (insert-fact (p-dbg (car child)) index node))))]
+             (hash-set tree-hash head (insert-fact (car child) index node))))]
       [(or `(,fact) (? symbol? fact) (? number? fact))
        (if (hash-has-key? tree-hash fact)
            ; if the hash-key exists and the child is a symbol
-           (hash-set tree-hash fact (cons (hash-ref tree-hash fact) index))
-           (let ([node `(,fact ,index)]) (hash-set tree-hash fact node)))]
+           (hash-set tree-hash fact (cons index (hash-ref tree-hash fact)))
+           (let ([node `(, index)]) (hash-set tree-hash fact node)))]
       ))
   (match tree
     [`(,tree-head ,tree-hash) `(,tree-head ,(insert-helper fact tree-hash index))]
@@ -55,20 +55,53 @@
   (define index-list (hash-keys line-hash #t))
   (foldl insert-fact root-tree facts-list index-list))
 
-(define (get-child tree-hash child)
-  (if (hash-has-key? tree-hash child)
-      (hash-ref tree-hash child)
-      #f)
+(define (get-leaves tree-hash)
+  ; does a traversal of the tree and return a list of all the leaves
+  ; the cadr for the parent of the leaf is the leaf value, have to check if the cadr is a number?,
+  ; if yes that is leaf value, return or add it to the list to be returned
+  ; if that is a hash, we call the function on that hash
+  (foldl (lambda (item index-list)
+           (match (hash-ref tree-hash item '())
+             [(or `(,item-tree-head ,(? hash? item-tree-hash))
+                  `(,item-tree-head ,(? hash? item-tree-hash) ,_))
+              (append (get-leaves item-tree-hash) index-list)]
+            ;  [(or `(,item-tree-head ,(? number? index))
+                  ; `(,item-tree-head ,(? number? index) ,_))
+             [(? list? node-index-list)
+              (append node-index-list index-list)] ; test when the tree only has one fact that only has one item, may be
+             ;  [_ '()]
+             ))
+         '()
+         (hash-keys tree-hash))
   )
+; takes a index list and gets the facts, but doesn't fill any sub-facts that are ref by index
+; (define (get-facts-by-index index-list)
+
+;   )
+
 
 ; search string should be in the format
-(define (search-facts tree search-str)
-  (define items (string-split search-str "/"))
-  (foldl (lambda (item tree)
-           ; check if the item exists in the tree-hash if it does return that, if it doesn't return #f
-           )
-           )
-  'todo)
+(define (search-facts tree items)
+  (define _ (p-dbg tree))
+  ; (define items (map string->symbol (string-split search-str "/")))
+  (define (get-sub-tree tree search-items)
+    (if (null? (p-dbg search-items))
+        tree
+        (match tree
+          [(or `(,tree-head ,(? hash? tree-hash)) `(,tree-head ,(? hash? tree-hash) ,_))
+           (if (hash-has-key? tree-hash (car search-items))
+               (get-sub-tree (hash-ref tree-hash (car search-items)) (cdr search-items))
+               '())]
+          )))
+  (match (p-dbg (get-sub-tree tree items))
+    [`(,_ ,(? hash? sub-tree-hash)) (get-leaves sub-tree-hash)]
+    [(? list? index-list) index-list]
+    ['() '()]
+    ))
 
-(search-facts 'todo "/apply/define/clo")
-; (slog-main "facts.txt")
+; (search-facts 'todo "/apply/define/clo")
+; (define ast-root (slog-main "facts_tests/facts_test4.txt"))
+(define ast-root (slog-main "facts_tests/facts.txt"))
+; (search-facts ast-root '(apply-prim))
+; (search-facts ast-root '(apply clo lambda fixedparam))
+(search-facts ast-root '(eval))
